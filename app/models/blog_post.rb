7604c7f04d51baa6afe0aa92ec6cf1b6deb0df06
Duggan Roberts <dugganroberts@gmail.com>
2019-10-24 15:30:32 -0500

    removed update_only, is not relavent for accepts_nested_attributes for our case

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -5,7 +5,7 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true
   belongs_to :blog_post_author
   has_many :blog_comment, dependent: :destroy
-  accepts_nested_attributes_for :blog_post_author, update_only: true
+  accepts_nested_attributes_for :blog_post_author
 
   def blog_post_author_attributes=(value)
     self.blog_post_author = BlogPostAuthor.find_or_create_by(value)
